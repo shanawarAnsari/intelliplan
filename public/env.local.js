f89001f612cc5c45e05c1bbca55f179673d6d4c8
W67917 <ahmadshanawar.ansari@kcc.com>
2025-11-13 16:47:48 +0530

    chatbot integration

---
+++ public/env.local.js
@@ -0,0 +1,5 @@
+window.__ENV__ = {
+    API_BASE_URL: "http://localhost:80/api",
+    OKTA_URL: "https://kcc.oktapreview.com",
+    OKTA_CLIENT_ID: "0oa2jeoztelhzt4r20h8"
+};
--- src/runtimeConfig.js
+++ src/runtimeConfig.js
@@ -2,5 +2,4 @@ export const CONFIG = {
     API_BASE_URL: window.__ENV__?.API_BASE_URL || '',
     OKTA_URL: window.__ENV__?.OKTA_URL || '',
     OKTA_CLIENT_ID: window.__ENV__?.OKTA_CLIENT_ID || '',
-    AGENT_URL: window.__ENV__?.AGENT_URL || '',
 };
